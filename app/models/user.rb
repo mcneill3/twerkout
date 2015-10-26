fd1b4b9a6d69375d3c1cb8faab958f4954be9f1c
Colin McNeill <colinpmcneill@gmail.com>
2015-10-25 21:38:16 -0400

    adding comment old code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    # where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
